2c7baa7c33be4d70b50d46be34202f7fde11d618
SashaPlavi <sasha.bingulac@gmail.com>
2020-01-23 17:40:37 +0100

    starting to stroke :)

---
+++ main.js
@@ -0,0 +1,46 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+console.log(canvas);
+
+/*ctx.fillStyle = "red";
+// rectangular
+ctx.fillRect(30, 30, 150, 100);
+ctx.fillStyle = "blue";
+ctx.fillRect(200, 30, 150, 100);
+
+//ouline
+ctx.strokeStyle = "red";
+ctx.strokeRect(30, 150, 150, 100);
+ctx.strokeStyle = "blue";
+ctx.strokeRect(200, 150, 150, 100);
+//clearRectangle
+ctx.clearRect(35, 35, 140, 90);
+
+ctx.fillText("halo world", 400, 30);
+*/
+
+//triangle
+/*
+ctx.beginPath();
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(100, 150);
+ctx.lineTo(50, 50);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(200, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(250, 150);
+ctx.lineTo(200, 50);
+ctx.stroke();
+
+// rectangle
+ctx.beginPath();
+ctx.moveTo(300, 50);
+ctx.lineTo(450, 50);
+ctx.lineTo(450, 150);
+ctx.lineTo(300, 150);
+ctx.lineTo(300, 50);
+ctx.stroke();
+*/
